33ea5e96424a95cdf276a881de0b8d8e9d3925c7
Tommy Chang <tommychang22@hotmail.com>
2023-08-30 00:44:59 -0400

    leetcode 8/30/23

--- Blind 75/Arrays & Hashing/product-of-array-except-self.java
+++ Blind 75/Arrays & Hashing/product-of-array-except-self.java
@@ -1,30 +1,39 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         /*
-            Idea is utilizing the array and calculating prefix/suffix
-            1. Can use two additional arrays to store left & right 
-                - (left[i] = left[i - 1] * nums[i - 1]) 
-                - (right[i] = right[i + 1] * nums[i + 1])
-                - then multiply left[i] and right[i] to create the res[i]
-            2. for o(1) space complexity, we can store the "curr" left and multiply it through the array, then proceed with the right. which is implemented below for o(n) runtime
+            Possible Solution 1:
+            Prefix Array * Suffix Array - Runtime O(N), Memory O(N)
+            1. initialize int[] prefix, and int[] suffix.
+            2. prefix[i] = nums[i-1] * prefix[i-1]; 
+            3. suffix[i] = nums[i+1] * suffix[i+1];
+            4. prefix[i] is every value multiplied to the left excluding prefix[i];
+            5. suffix[i] is every value multiplied to the right excluding suffix[i];
+            6. ans[i] = prefix[i] * suffix[i] 
 
+            Possible Solution 2:
+            Fill answer array with prefix and then suffix - Runtime O(N), Memory O(N)
+            1. initialize curr = 1;
+            2. fill ans array with 1 (java initializes array with 0)
+            3. ans[i] *= curr (prevNum (to the left/right));
+            4. curr *= nums[i] (nextNum(to the left/right));
+            5. repeat for suffix
         */
-        int res[] = new int[nums.length];
+        int n  = nums.length;
         int curr = 1;
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans, 1);
 
-        Arrays.fill(res, 1);
-        
-        for (int i = 0; i < nums.length; i++){
-            res[i] *= curr;
+        for (int i = 0; i < n; i++){
+            ans[i] *= curr;
             curr *= nums[i];
         }
+
         curr = 1;
-        for (int i = nums.length - 1; i >= 0; i--){
-            res[i] *= curr;
+        for (int i = n - 1; i >= 0; i--){
+            ans[i] *= curr;
             curr *= nums[i];
         }
-
-        return res;
-
+        return ans;
     }
+
 }
